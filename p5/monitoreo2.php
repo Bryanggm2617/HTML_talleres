4c1a0e835b957e18a9f4cc47d2410cbe221ca2cb
Bryanggm2617 <74894661+Bryanggm2617@users.noreply.github.com>
2021-06-30 23:27:04 -0500

    Create monitoreo2.php

---
+++ p5/monitoreo2.php
@@ -0,0 +1,13 @@
+<?php
+
+$temperatura = $_POST ['temp'];
+$voltaje = $_POST ['volt'];
+$humedad = $_POST ['hum'];
+
+
+
+echo $temperatura."<br>";
+echo $voltaje."<br>";
+echo $humedad."<br>";
+
+?>
