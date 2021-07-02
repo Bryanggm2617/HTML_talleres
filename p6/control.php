efff8d21290f3f5096cbf95a56866598ac7d3f27
Bryanggm2617 <74894661+Bryanggm2617@users.noreply.github.com>
2021-07-02 13:44:31 -0500

    format
    d

--- p6/control.php
+++ p6/control.php
@@ -1,13 +1,11 @@
 <?php
-echo "<h2>1</h2><br>";
+// echo "<h2>1</h2><br>";
 include_once( $_SERVER['DOCUMENT_ROOT'] ."p6". "/config.inc.php" );
-echo "<h2>2</h2><br>";
+// echo "<h2>2</h2><br>";
 include_once(DIR_INC . "class.mysql.inc.php");
-echo "<h2>3</h2><br>";
+// echo "<h2>3</h2><br>";
 $bd = new class_mysql();
-echo "<h2>4</h2><br>";
+//echo "<h2>4</h2><br>";
 $bd->insertar($_GET['temp'] , $_GET['volt'], $_GET['hum']);
-echo "<h2>5</h2><br>";
-
-
+//echo "<h2>5</h2><br>";
 ?>
