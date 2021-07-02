8b7e002de9078d10b7ad80e83bc4a1c86f9bb7bc
Bryanggm2617 <74894661+Bryanggm2617@users.noreply.github.com>
2021-07-02 13:27:20 -0500

    Create config.inc.php

---
+++ p6/config.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+	define( "DIR_RAIZ", $_SERVER['DOCUMENT_ROOT']."p6" );
+	define( "DIR_INC", DIR_RAIZ . "/includes/" );
+	
+	define( "HOST", "http://" . $_SERVER['HTTP_HOST'] );
+	define( "DIR_IMG", HOST . "/images/" );
+
+	define( "MYSQL_HOST", "localhost" );
+	define( "MYSQL_USER", "user1" );
+	define( "MYSQL_PASSWD", "toortoor" );
+	define( "MYSQL_DBNAME", "practicas" );
+
+
+	echo $_SERVER['DOCUMENT_ROOT']."<br>";
+	echo DIR_RAIZ."<br>";
+	echo TMPDIR."<br>";
+	echo HOST."<br>";
+	echo DIR_INC."<br>";
+	echo DIR_IMG."<br>";
+	echo MYSQL_HOST;
+
+?>
